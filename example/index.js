c4f2abd4304a7a74a97ed6846cebb964f0d976c7
peakchen90 <1558267774@qq.com>
2017-04-13 12:15:56 +0800

    modify example

--- example/index.js
+++ example/index.js
@@ -2,10 +2,21 @@ var box = document.getElementById('box')
 
 var g = new Gesture(box)
 
+function getStyle(el, attr) {
+  if (el && el.currentStyle) {
+    return el.currentStyle[attr]
+  } else {
+
+    console.log(window.getComputedStyle(el)[attr])
+    return window.getComputedStyle(el)[attr]
+  }
+}
+
 g.ondrag(function (e) {
-  var boxCss = window.getComputedStyle(box)
-  var top = boxCss.top.replace(/px/, '') - 0
-  var left = boxCss.left.replace(/px/, '') - 0
+  console.log(e.gesture)
+  var top = getStyle(box, 'top').replace(/px/, '') - 0
+  var left = getStyle(box, 'left').replace(/px/, '') - 0
+  console.log(top, left)
   var deltaX = e.gesture.deltaX
   var deltaY = e.gesture.deltaY
   box.style.top = top + deltaY + 'px'
